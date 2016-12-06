ff0ce7a92e9ced7cf6ef05f20f5feff28df2d6a9
mir.mahdi <mirouf16@gmail.com>
2016-12-06 10:56:16 +0100

    Signed-off-by: mir.mahdi <mirouf16@gmail.com>

--- src/main/java/soa/webservice/NotebookService.java
+++ src/main/java/soa/webservice/NotebookService.java
@@ -6,7 +6,10 @@ import javax.jws.WebService;
 @WebService(name = "NotebookService", targetNamespace="http://soa.webservice")
 public interface NotebookService {
 	boolean addPerson(Person p);
-
+/**
+ * liste des personnes
+ * @return
+ */
 	List<Person> getPersons();
 
 	Person getPersonAt(String name);
